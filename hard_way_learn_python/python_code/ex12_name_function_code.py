e8ee178ca8e66f777c0266aad26e2d7bc80a16fe
CraftHeart <1247751874@qq.com>
2017-05-24 22:11:41 +0800

    update

--- hard_way_learn_python/python_code/ex12_name_function_code.py
+++ hard_way_learn_python/python_code/ex12_name_function_code.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 # When you start to define a function , you need to consider:
 """
     1.Did you start your function definition with def?
@@ -22,6 +23,9 @@
  * tells python to take all the arguments to the function and then put them in
  args as a list.
 """
+
+# 不可变类型参数：类似于C++中的值传递
+# 可变类型参数：类似于C++中的引用传递
 def print_two(*args):
     # interprete the arguments
     arg1,arg2 = args
@@ -45,3 +49,35 @@ print_two(arg1,arg2)
 print_two_again(arg1,arg2)
 print_one(arg1)
 print_non()
+
+# 匿名函数
+# lambda函数的语法只包含一个语句，如下：
+# lambda [arg1,[,arg2,...argn]]:expression
+
+sum_num = lambda arg1,arg2: arg1+arg2
+print sum_num(1,2)
+
+
+# 全局变量问题
+total = 0
+
+def add(arg1,arg2):
+    # 数字数据类型不可改变，这里仅仅是对total的一个拷贝进行了赋值
+    total = arg1+arg2
+    print "total in function is ",total
+    return total
+
+add(10,20)
+# result 0
+print total
+
+def add_plus(arg1,arg2):
+    # 声明为全局变量，下面即将改变它的值
+    global total
+    total = arg1 + arg2
+    print "total in function is ",total
+    return total
+
+add_plus(10,20)
+# result 30
+print total
--- hard_way_learn_python/python_code/ex_write.py
+++
@@ -1,5 +0,0 @@
-
-my_name = "i am \n bobo."
-print my_name
-
-print "my_name is %r" %my_name
