7685640620b2dc367d4e92295936467b6f959215
CraftHeart <1247751874@qq.com>
2017-06-09 10:33:55 +0800

    "update webcam"

--- pyhton_in_action/webCam/captureSend.py
+++ pyhton_in_action/webCam/captureSend.py
@@ -1,26 +1,49 @@
+'''
+    Web Camera Transporting
+    Author:Cuber Qiu
+    2017-06-09
+
+    This is Server:
+    Using pyhton capture usb camera data , and decoding it into jpeg and splict
+    it into small fragmentations, then send them to client
+    I add a function:
+    we can collect data from two camera switching by clicking 1 or 0.
+'''
+
 import numpy as np
 import cv2
 from socket import *
-import time
 
+# camera0
 cap0 = cv2.VideoCapture(0)
+# camera1
 cap1 = cv2.VideoCapture(1)
+
+# setting camera0's picture size
 cap0.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 cap0.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+# setting camera1's picture size
 cap1.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 cap1.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 
-
+# UDP initialization
 s = socket(AF_INET,SOCK_DGRAM)
+# destination ip
 host = '127.0.0.1'
+# destination port
 port = 9996
+# address must be setted as a tuple
 addr = (host,port)
 
 # cv2.namedWindow('show',cv2.WND_PROP_FULLSCREEN)
+# read camera data
 ret,frame0 = cap0.read()
 ret,frame1 = cap1.read()
 
+# if show_0 == True ,send camera0's data
+# if show_0 == False ,send camera1's data
 show_0 = True
+# which keyboard u clicked, initialized with '0'
 key = '0'
 
 while True:
@@ -28,16 +51,22 @@ while True:
         cv2.imshow('show',frame0)
         key = cv2.waitKey(1)
 
+        # encode frame0 into jpeg
         ret,buff = cv2.imencode('.jpg',frame0,[int(cv2.IMWRITE_JPEG_QUALITY),75])
+        # change the buff into array
         data = np.array(buff)
+        # convert data to string
         stringdata = data.tostring()
         print len(stringdata)
     else:
         cv2.imshow('show',frame1)
         key = cv2.waitKey(1)
 
+        # encode frame1 into jpeg
         ret,buff = cv2.imencode('.jpg',frame1,[int(cv2.IMWRITE_JPEG_QUALITY),75])
+        # convert buff to array
         data = np.array(buff)
+        # conver array to string
         stringdata = data.tostring()
         print len(stringdata)
 
@@ -46,7 +75,7 @@ while True:
     elif key == ord('0'):
         show_0 = True
 
-
+    # sending data to client
     count = 0
     max_size = 60000
     while count < len(stringdata):
--- pyhton_in_action/webCam/receiver.py
+++ pyhton_in_action/webCam/receiver.py
@@ -1,3 +1,11 @@
+'''
+       Web Camera Transporting
+       Author:Cuber Qiu
+       2017-06-09
+
+       This is Client:
+       Using pyhton UDP to receive camera data from server.
+'''
 import numpy as np
 import cv2
 import socket
@@ -39,9 +47,13 @@ while True:
     print data_show
     print data_show[0]
     print data_show[(len(data_show)-2):]
+    # checking if the frame is a standard jpeg picture
+    # if it is a standard jepg picture,
+    # the first 2 byte will be 0xFF 0xD8, the last 2 byte will be 0xFF 0xD9
     if data_show[0]==0xff and data_show[1]==0xd8 and \
     data_show[len(data_show)-2]==0xff and data_show[len(data_show)-1]==0xd9:
         # print len(data_show)
+        # decode the data
         img_decoded = cv2.imdecode(data_show,1)
         # print img_decoded
 
